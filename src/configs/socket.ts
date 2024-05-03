46c4b47b328a97398fe24eeabe3dd3c0ae212e4b
David <bischof.david.db@gmail.com>
2024-05-03 23:28:47 +0200

    merge and changed the .env thingies

--- src/configs/socket.ts
+++ src/configs/socket.ts
@@ -1,5 +1,5 @@
 import { io } from 'socket.io-client';
 
-const URL: string = import.meta.env.VITE_BACKEND_URL === 'production' ? undefined : 'http://localhost:3000';
+const URL: string = import.meta.env.VITE_BACKEND_URL;
 
 export const socket = io(URL);
